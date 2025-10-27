bf969cd8b7e264273fde9fd855e07a2684853aa9
Yasodha Dollu <yasodha.dollu@gmail.com>
2025-10-27 11:49:28 +0530

    feat: fix imports

---
+++ packages/react/src/client.ts
@@ -0,0 +1,8 @@
+'use client'
+
+// Client-side exports only
+export * from './types'
+export * from './hooks'
+export * from './provider'
+export * from './query'
+export * from './components'
--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,8 +1,2 @@
-// server
-export * from './helpers'
-export * from './types'
-// client
-export * from './hooks'
-export * from './provider'
-export * from './query'
-export * from './components'
+export * from './server'
+export * from './client'
